17948ca2f8a00a70e1fbb70d3c53cc8aa2d6ad5f
Ommatidia <ommatidia@gmail.com>
2012-09-10 05:43:20 -0400

    added command line options for filetypes to formatPath util

--- scripts/formatPath.py
+++ scripts/formatPath.py
@@ -8,13 +8,29 @@ usage: ls -R [DIR] | python formatPath.py 1> selected.txt 2> discarded.txt
      : ls -R [DIR] | python formatPath.py > selected.txt 2> /dev/null
 """
 
-import os, sys
+import os, sys, argparse
 
-#file_selectors = ['.tif', '.png', '.gif', '.jpg']
-file_selectors = ['.tif']
+def get_parser():
+    title = "Utility for listing paths of all files recursively in a directory that match by file type"
+    parser = argparse.ArgumentParser(title)
+    
+    #TODO: better help docs
 
-currentPath = None
+    parser.add_argument(dest='ext', nargs='*', default=['.tif'], \
+                            help="File extensions, default is '.tif'") 
+
+    return parser
+
+
+#TODO: error handling for invalid inputs
+
+
 if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+
+    file_selectors = args.ext
+    currentPath = None
     for line in sys.stdin:
         line = line.strip()
         
